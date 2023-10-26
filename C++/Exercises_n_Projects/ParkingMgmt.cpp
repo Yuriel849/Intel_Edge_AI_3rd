5e37d412a350480060f7f4fd01843337140577ce
Yuriel849 <yuriel849sp@gmail.com>
2023-10-26 16:46:38 +0900

    과제: 주차요금 계산 함수_캡슐, 완료

--- C++/Exercises_n_Projects/ParkingMgmt.cpp
+++ C++/Exercises_n_Projects/ParkingMgmt.cpp
@@ -18,7 +18,9 @@ int main()
 	cout << "Welcome to our parking lot XXX (Lot No. 0123)" << endl;
 	cout << "The fees are 2000 KRW per hour, 1000 KRW per 30 minutes, and 400 KRW per 10 minutes. \nThe minimum fee is 400 KRW, even for a stay of less than ten minutes." << endl;
 
-	Car car1("ABCD1234", "20231023163404", "20231025145019", 0123, 0);
+	//Car car1("ABCD1234", "20231023163404", "20231025145019", 0123, 0);
+
+	Car car1("ABCD1234", "20231023163404", "20231023170019", 0123, 1);
 	cout << "Your car arrived at " << car1.getHour(1) << " hours " << car1.getMinute(1) << " minutes " << car1.getSecond(1) << " seconds on "
 		<< car1.getYear(1) << "." << car1.getMonth(1) << "." << car1.getDay(1) << "." << endl;
 	cout << "Your car departed at " << car1.getHour(0) << " hours " << car1.getMinute(0) << " minutes " << car1.getSecond(0) << " seconds on "
@@ -65,14 +67,29 @@ int calculateFee(Car car)
 	// Calculate fee based on the lenght of stay at this parking lot.
 	if (minutes > 60) // If stay is longer than one hour
 	{
-		fee	= (minutes / 2000)
+		// The fees are 2000 KRW per hour, 1000 KRW per 30 minutes, and 400 KRW per 10 minutes. \nThe minimum fee is 400 KRW, even for a stay of less than ten minutes
 
+		fee += (minutes / 60) * 2000;
+		minutes = minutes % 60;
 	}
-	else if(minutes > 30) // If stay is longer than 30 minutes but less than one hour (60 minutes)
+	else if(minutes >= 30) // If stay is longer than 30 minutes but less than one hour (60 minutes)
 	{
-
+		fee += (minutes / 30) * 1000;
+		minutes = minutes % 30;
+	}
+	else if(minutes >= 10) // If stay is longer than 10 minutes but less than 30 minutes
+	{
+		fee += (minutes / 10) * 400;
+	}
+	else // If stay is less than 10 minutes
+	{
+		fee = 400;
 	}
 
+	if (car.getDisabled() == 1)
+	{
+		fee *= 0.5;
+	}
 
-	return 0;
+	return fee;
 }
