a9764a859f25990c73abaa9de4f37c4721199d7b
Yuriel849 <yuriel849sp@gmail.com>
2023-10-27 10:15:01 +0900

    MFC dialog example

---
+++ C++/MFC/DlgTest/DlgTestView.h
@@ -0,0 +1,51 @@
+﻿
+// DlgTestView.h: CDlgTestView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CDlgTestView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CDlgTestView() noexcept;
+	DECLARE_DYNCREATE(CDlgTestView)
+
+// 특성입니다.
+public:
+	CDlgTestDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CDlgTestView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnIddDialog1();
+};
+
+#ifndef _DEBUG  // DlgTestView.cpp의 디버그 버전
+inline CDlgTestDoc* CDlgTestView::GetDocument() const
+   { return reinterpret_cast<CDlgTestDoc*>(m_pDocument); }
+#endif
+
---
+++ C++/MFC/DlgTest/NameDlg.cpp
@@ -0,0 +1,46 @@
+﻿// NameDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "DlgTest.h"
+#include "afxdialogex.h"
+#include "NameDlg.h"
+
+
+// CNameDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CNameDlg, CDialogEx)
+
+CNameDlg::CNameDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CNameDlg::~CNameDlg()
+{
+}
+
+void CNameDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CNameDlg, CDialogEx)
+	ON_EN_CHANGE(IDC_EDIT1, &CNameDlg::OnEnChangeEdit1)
+END_MESSAGE_MAP()
+
+
+// CNameDlg 메시지 처리기
+
+
+void CNameDlg::OnEnChangeEdit1()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CDialogEx::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
