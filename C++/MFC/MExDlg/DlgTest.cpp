8e6b46c9c375434fd80c65121294b374c69cfcde
Yuriel849 <yuriel849sp@gmail.com>
2023-10-27 14:03:44 +0900

    Dialog button press messages example

---
+++ C++/MFC/MExDlg/DlgTest.cpp
@@ -0,0 +1,71 @@
+﻿// DlgTest.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MExDlg.h"
+#include "afxdialogex.h"
+#include "DlgTest.h"
+
+
+// CDlgTest 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgTest, CDialog)
+
+CDlgTest::CDlgTest(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIGTEST, pParent)
+	, m_bCheckButton(FALSE)
+{
+
+}
+
+CDlgTest::~CDlgTest()
+{
+}
+
+void CDlgTest::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Check(pDX, IDC_CHECK1, m_bCheckButton);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgTest, CDialog)
+	ON_BN_CLICKED(IDC_RADIO1, &CDlgTest::OnBnClickedRadio1)
+	ON_BN_CLICKED(IDC_RADIO2, &CDlgTest::OnBnClickedRadio2)
+	ON_BN_CLICKED(IDC_CHECK1, &CDlgTest::OnBnClickedCheck1)
+	ON_BN_CLICKED(IDC_BUTTON1, &CDlgTest::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CDlgTest 메시지 처리기
+
+
+void CDlgTest::OnBnClickedRadio1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	AfxMessageBox(_T("Radio button 1 press"));
+}
+
+
+void CDlgTest::OnBnClickedRadio2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	AfxMessageBox(_T("Radio button 2 press"));
+}
+
+
+void CDlgTest::OnBnClickedCheck1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	CString message;
+	message.Format(_T("Check button press value : %d"), m_bCheckButton);
+	AfxMessageBox(message);
+}
+
+
+void CDlgTest::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	AfxMessageBox(_T("Button 1 press"));
+}
---
+++ C++/MFC/MExDlg/DlgTest.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CDlgTest 대화 상자
+
+class CDlgTest : public CDialog
+{
+	DECLARE_DYNAMIC(CDlgTest)
+
+public:
+	CDlgTest(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgTest();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIGTEST };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedRadio1();
+	afx_msg void OnBnClickedRadio2();
+	BOOL m_bCheckButton;
+	afx_msg void OnBnClickedCheck1();
+	afx_msg void OnBnClickedButton1();
+};
