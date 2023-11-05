387569901afc420564e9d5b0549731a2c9ad985b
Yuriel849 <yuriel849sp@gmail.com>
2023-11-06 00:40:49 +0900

    MFC RFID project, MariaDb connected

---
+++ C++/RFID/RFID/datasource.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef DATASOURCE_H
+#define DATASOURCE_H
+
+#include <iostream>
+#include <mysql.h>
+
+using namespace std;
+
+class DataSource
+{
+public:
+	void db_connect_test();
+};
+
+#endif
