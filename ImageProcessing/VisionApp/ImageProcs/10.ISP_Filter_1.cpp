499198c001ee2a62c5ce2729fb3a188620c6a9ed
Yuriel849 <yuriel849sp@gmail.com>
2023-11-07 16:37:32 +0900

    Add vector example

--- ImageProcessing/VisionApp/ImageProcs/10.ISP_Filter_1.cpp
+++ ImageProcessing/VisionApp/ImageProcs/10.ISP_Filter_1.cpp
@@ -6,14 +6,20 @@
 
 int main()
 {
+	// 노이즈가 없는 대표 신호 값을 정하시오. 정렬 후 중간값을 구하라 (신호처리에 매우 효과적이면서도 매우 간단한 알고리즘이다. 단점은 반복 사용시 시간이 오래걸린다.).
 	int datas[] = { 6,4,2,5,9,8,4,5,2,1,5,3,6,5,2,1,1,4,5,2,23,3,6,6,1248,2,56,6,105,112 };
 	int length = sizeof(datas) / sizeof(datas[0]); // 30
-
-	// 노이즈가 없는 대표 신호 값을 정하시오. 정렬 후 중간값을 구하라 (신호처리에 매우 효과적이면서도 매우 간단한 알고리즘이다. 단점은 반복 사용시 시간이 오래걸린다.).
 	sort(datas, datas + length);
-	cout << datas[length / 2] << endl;
 
+	// vector 사용
+	std::vector<int> vDatas(datas, datas + length);
+	std::sort(vDatas.begin(), vDatas.end());
 
+	int major = datas[length / 2];
+	int vMajor = vDatas[length / 2];
 
-	return 0;
+	cout << major << endl;
+	cout << vMajor << endl;
+
+	return 1;
 }
