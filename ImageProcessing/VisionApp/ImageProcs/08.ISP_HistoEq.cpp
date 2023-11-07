c8cd20383c0addd08affd4a7b47d3f6770099a88
Yuriel849 <yuriel849sp@gmail.com>
2023-11-07 13:37:58 +0900

    ISP Histogram Equalization code

--- ImageProcessing/VisionApp/ImageProcs/08.ISP_HistoEq.cpp
+++ ImageProcessing/VisionApp/ImageProcs/08.ISP_HistoEq.cpp
@@ -11,51 +11,21 @@ int main()
 	int length = src.total();//data length
 	int channels = src.channels();
 
-	//cv::Mat gray_cv = cv::Mat(src.rows, src.cols, CV_8UC1);
-	cv::Mat gray = cv::Mat(src.rows, src.cols, CV_8UC1);
-
-	//cv::cvtColor(src, gray_cv, ColorConversionCodes::COLOR_BGR2GRAY);
-
 	ISP _isp;
-	_isp.Convert_BGR2GRAY(src.data, src.cols, src.rows, gray.data);
 
-	//Mat diff = gray_cv - gray;
-
-	const int histoSz = 256;
-	int histo[histoSz] = { 0, };
-	_isp.Get_Histogram(gray.data, gray.cols, gray.rows, histo, histoSz);
-
-	////확률밀도함수
-	//int acc[histoSz] = { 0, };
-	////acc[0] = histo[0]+ histo[1]
-	//acc[0] = histo[0];
-	//for (size_t i = 1; i < histoSz; i++)
-	//{
-	//	acc[i] = acc[i - 1]/*histo[i-1]*/ + histo[i];
-	//}
-
-	////new Look Up Table ... 영상의 각 화소 값들을 새로운 대응값으로 맵핑
-	//int new_Gray[histoSz] = { 0, };
-	//for (size_t i = 0; i < histoSz; i++)
-	//{
-	//	new_Gray[i] = static_cast<int>(1.0 * acc[i] * 255 / length);
-	//}
-
-	////assign new Gray value using LUT
-	//cv::Mat gray_Eq = cv::Mat(src.rows, src.cols, CV_8UC1);
-	//for (size_t i = 0; i < length; i++)
-	//{
-	//	gray_Eq.data[i] = new_Gray[gray.data[i]];
-	//}
-
+	//origin_image
+	cv::Mat gray = cv::Mat(src.rows, src.cols, CV_8UC1);
+	//enhance_image using HistoEq
 	cv::Mat gray_Eq = cv::Mat(src.rows, src.cols, CV_8UC1);
 
+	_isp.Convert_BGR2GRAY(src.data, src.cols, src.rows, gray.data);
 	_isp.Enhance_HistogramEq(
 		gray.data,
 		gray.cols,
 		gray.rows,
 		gray_Eq.data
 	);
+
 
 	//library OpenCV
 	//check correlation
@@ -68,7 +38,9 @@ int main()
 		Mat diff = gray_Eq - gray_Eq_cv;
 		double error = cv::sum(diff)[0];
 		int a = 0;
+
 	}
+
 
 	return 1;
 }
--- ImageProcessing/VisionApp/ImageProcs/ISP.cpp
+++ ImageProcessing/VisionApp/ImageProcs/ISP.cpp
@@ -1,4 +1,4 @@
-#include "ISP.h"
+Ôªø#include "ISP.h"
 
 ISP::ISP()
 {
@@ -10,54 +10,69 @@ ISP::~ISP()
 	std::cout << "ISP::Dtor" << std::endl;
 }
 
-bool ISP::Convert_BGR2GRAY(uchar* pBGR, int width, int height, uchar* pGray)
+bool ISP::Convert_BGR2GRAY(uchar* pBGR, int cols, int rows, uchar* pGray)
 {
-	if (pBGR == nullptr || pGray == nullptr) { return false; }
-
-#if 1
-	Mat src = Mat(width, height, CV_8UC3, pGray);
-	Mat gray_cv = Mat(width, height, CV_8UC1, pGray);
-	cvtColor(src, gray_cv, COLOR_BGR2GRAY);
-#else // Code above gives better performance
-	for (size_t row = 0; row < width; row++)
+	if (pBGR == nullptr || pGray == nullptr)
 	{
-		for (size_t col = 0; col < height; col++)
-		{
-			int index = (row) * height + (col);
-			int index_B = index * 3 + 0;
-			int index_G = index * 3 + 1;
-			int index_R = index * 3 + 2;
-			int val_B = pBGR[index_B];
-			int val_G = pBGR[index_G];
-			int val_R = pBGR[index_R];
+		return false;
+	}
 
-			int val_Y = 0.299 * val_R + 0.587 * val_G + 0.114 * val_B;
+	Mat src = Mat(rows, cols, CV_8UC3, pBGR);
+	Mat gray_cv = Mat(rows, cols, CV_8UC1, pGray);
+	cvtColor(src, gray_cv, COLOR_BGR2GRAY);
+
+	return true;
+
+
+	for (size_t row = 0; row < rows; row++)
+	{
+		for (size_t col = 0; col < cols; col++)
+		{
+			int index = (row)*cols + (col);
+			//Y‚Üê0.299‚ãÖR+0.587‚ãÖG+0.114‚ãÖB
+			float val_Y =
+				0.299 * pBGR[index * 3 + 2]//r
+				+ 0.587 * pBGR[index * 3 + 1]//g
+				+ 0.114 * pBGR[index * 3 + 0];//b
 			pGray[index] = (uchar)(val_Y + 0.5);
+
 		}
 	}
-#endif
+
+
 	return true;
 }
 
 bool ISP::Get_Histogram(uchar* pGray, int cols, int rows, int* pHisto, int histoSz)
 {
-	if (pGray == nullptr || pHisto == nullptr) { return false; }
+	if (pGray == nullptr || pHisto == nullptr)
+	{
+		return false;
+	}
+	//if (sizeof(uchar) != histoSz)
+	//{
+	//	return false;
+	//}
 
 	for (size_t i = 0; i < cols * rows; i++)
 	{
-		if (pGray[i] >= histoSz) continue; // size 256
+		if (pGray[i] >= histoSz)continue;//size 256
 		pHisto[pGray[i]]++;
 	}
 
-	return true;
+
+	return false;
 }
 
 bool ISP::Enhance_HistogramEq(uchar* pGray, int cols, int rows, uchar* pGrayEq)
 {
+	int length = cols * rows;
 	const int histoSz = 256;
-	//»Æ∑¸π–µµ«‘ºˆ
-	int acc[histoSz] = { 0, };
 	int histo[histoSz] = { 0, };
+	Get_Histogram(pGray, cols, rows, histo, histoSz);
+
+	//ÌôïÎ•†Î∞ÄÎèÑÌï®Ïàò
+	int acc[histoSz] = { 0, };
 	//acc[0] = histo[0]+ histo[1]
 	acc[0] = histo[0];
 	for (size_t i = 1; i < histoSz; i++)
@@ -65,18 +80,17 @@ bool ISP::Enhance_HistogramEq(uchar* pGray, int cols, int rows, uchar* pGrayEq)
 		acc[i] = acc[i - 1]/*histo[i-1]*/ + histo[i];
 	}
 
-	//new Look Up Table ... øµªÛ¿« ∞¢ »≠º“ ∞™µÈ¿ª ªı∑ŒøÓ ¥Î¿¿∞™¿∏∑Œ ∏ «Œ
+	//new Look Up Table ... ÏòÅÏÉÅÏùò Í∞Å ÌôîÏÜå Í∞íÎì§ÏùÑ ÏÉàÎ°úÏö¥ ÎåÄÏùëÍ∞íÏúºÎ°ú ÎßµÌïë
 	int new_Gray[histoSz] = { 0, };
 	for (size_t i = 0; i < histoSz; i++)
 	{
-		new_Gray[i] = static_cast<int>(1.0 * acc[i] * 255 / (rows*cols));
+		new_Gray[i] = static_cast<int>(1.0 * acc[i] * 255 / length);
 	}
 
-	//assign new Gray value using LUT
-	cv::Mat gray_Eq = cv::Mat(rows, cols, CV_8UC1);
-	for (size_t i = 0; i < (rows*cols); i++)
+
+	for (size_t i = 0; i < length; i++)
 	{
-		gray_Eq.data[i] = new_Gray[pGray[i]];
+		pGrayEq[i] = new_Gray[pGray[i]];
 	}
 
 	int histo_new[histoSz] = { 0, };
