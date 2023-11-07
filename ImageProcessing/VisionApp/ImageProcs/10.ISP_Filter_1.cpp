47d321482016afc523af23f9faa959ca8dd0a090
Yuriel849 <yuriel849sp@gmail.com>
2023-11-07 17:01:31 +0900

    Applying ISP example

--- ImageProcessing/VisionApp/ImageProcs/10.ISP_Filter_1.cpp
+++ ImageProcessing/VisionApp/ImageProcs/10.ISP_Filter_1.cpp
@@ -21,5 +21,38 @@ int main()
 	cout << major << endl;
 	cout << vMajor << endl;
 
+	std::string fileName = "../thirdparty/opencv_480/sources/samples/data/lena.jpg";
+	cv::Mat src_gray = cv::imread(fileName, cv::ImreadModes::IMREAD_GRAYSCALE);
+
+	cv::Mat src_gray_blur = src_gray.clone();
+	src_gray_blur = 0;
+
+	// filter 3x3 ... 1/9
+	const int filter_sz = 3;
+	float blur[] = {
+		1.0 / 9, 1.0 / 9, 1.0 / 9,
+		1.0 / 9, 1.0 / 9, 1.0 / 9,
+		1.0 / 9, 1.0 / 9, 1.0 / 9,
+	};
+
+	for (size_t row = 0; row < src_gray.rows - filter_sz; row++)
+	{
+		for (size_t col = 0; col < src_gray.cols - filter_sz; col++)
+		{
+			int sum = 0;
+			for (size_t f_row = 0; f_row < filter_sz; f_row++)
+			{
+				for (size_t f_col = 0; f_col < filter_sz; f_col++)
+				{
+					int index = (row + f_row) * src_gray.cols + (col + f_col);
+					int f_index = (f_row)*filter_sz + (f_col);
+					sum = src_gray.data[index] + blur[f_index];
+				}
+			}
+			int index = (row)*src_gray.cols + (col);
+			src_gray_blur.data[index] = static_cast<uchar>(sum);
+		}
+	}
+
 	return 1;
 }
