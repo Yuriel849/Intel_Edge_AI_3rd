af5e3f253a47d455bf3a4b540acbeb44efbe3de1
DESKTOP-FA1PHE5\USER <yuriel849sp@gmail.com>
2023-11-17 12:00:44 +0900

    Ampoules WIP

--- ImageProcessing/VisionApp/ImageProcs/21.ISP_Ampoules_ex.cpp
+++ ImageProcessing/VisionApp/ImageProcs/21.ISP_Ampoules_ex.cpp
@@ -4,13 +4,15 @@
 
 int main()
 {
+	int ampoule_max = 95, ampoule_min = 135;
+
 	std::string fileName = "../KCCImageNet/images/ampoules/ampoules_01.png";
 	cv::Mat src = cv::imread(fileName, cv::ImreadModes::IMREAD_ANYCOLOR);
 	cv::Mat src_gray = cv::imread(fileName, cv::ImreadModes::IMREAD_GRAYSCALE);
 
 	// 이진화
 	cv::Mat src_bin, src_open;
-	cv::threshold(src_gray, src_bin, 220, 255, ThresholdTypes::THRESH_BINARY);
+	cv::threshold(src_gray, src_bin, 210, 255, ThresholdTypes::THRESH_BINARY);
 	//inRange(src_gray, Scalar(240), Scalar(255), src_bin);
 
 	// 형태학적 처리 (노이즈 제거)
@@ -27,29 +29,22 @@ int main()
 	}
 
 	// contours 잡기
-
 	RNG rng(12345);
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	findContours(src_open, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-	//Mat drawing;
 
-	//if (src_gray.channels() == 3) { drawing = src_gray.clone(); }
-	//else { cvtColor(src_gray, drawing, COLOR_GRAY2BGR); }
-
-	cv::Mat src_masked = cv::Mat::zeros(src_gray.size(), CV_8UC1);
+	cv::Mat src_masked = src_gray.clone();
 	uchar* pMData = src_masked.data;
 	for (size_t i = 0; i < contours.size(); i++) //꼭지점 사이즈만큼 i 반복
 	{
-
-		//Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-		//drawContours(drawing, contours, (int)i, color, FILLED);
 		RotatedRect rrt = minAreaRect(contours[i]);
 		int max_y = 60, min_y = 230;
-		line(src_gray, Point(rrt.center.x+20, 0), Point(rrt.center.x+20, src_gray.rows), CV_RGB(0, 0, 255));
-		line(src_gray, Point(rrt.center.x-20, 0), Point(rrt.center.x-20, src_gray.rows), CV_RGB(255, 0, 0));
-		line(src_gray, Point(0, max_y), Point(src_gray.cols, max_y), CV_RGB(0, 255, 255));
-		line(src_gray, Point(0, min_y), Point(src_gray.cols, min_y), CV_RGB(255, 0, 255));
+
+		line(src_gray, Point(rrt.center.x+20, 0), Point(rrt.center.x+20, src_gray.rows), 0);
+		line(src_gray, Point(rrt.center.x-20, 0), Point(rrt.center.x-20, src_gray.rows), 0);
+		line(src_gray, Point(0, max_y), Point(src_gray.cols, max_y), 0);
+		line(src_gray, Point(0, min_y), Point(src_gray.cols, min_y), 0);
 
 		uchar* pData = src_gray.data;
 
@@ -59,17 +54,70 @@ int main()
 			for (size_t iy = 0; iy < src_gray.rows; iy++)
 			{
 				int index = iy * src_gray.cols + ix;
-				if ((ix < rrt.center.x + 20 || rrt.center.x - 20 < ix) && (iy < max_y || min_y < iy))
+				if ((rrt.center.x - 20 < ix) && (ix < rrt.center.x + 20))
 				{
 					pMData[index] = 0;
 				}
-				else
+				else if (iy < max_y || min_y < iy)
+				{
+					pMData[index] = 0;
+				}
+				else if(pMData[index] != 0)
 				{
 					pMData[index] = pData[index];
 				}
 			}
 		}
-		int a = 0;
+		int a = 0; // Anchor to set breakpoint
+	}
+
+	Mat src_eqa = src_masked.clone();
+	contours.clear();
+	hierarchy.clear();
+	findContours(src_masked, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	for (size_t i = 0; i < contours.size()-1; i++)
+	{
+		RotatedRect rrt = minAreaRect(contours[i]);
+		Mat subROIimg = src_masked(rrt.boundingRect());
+		if (rrt.size.height >= 34)
+		{
+			Mat subROIimgEq;
+			equalizeHist(subROIimg, subROIimgEq);
+			subROIimgEq.copyTo(src_eqa(rrt.boundingRect()));
+		}
+		int a = 0; // Anchor to set breakpoint
+	}
+
+	contours.clear();
+	hierarchy.clear();
+	findContours(src_eqa, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	for (size_t i = 0; i < contours.size(); i++) //꼭지점 사이즈만큼 i 반복
+	{
+		RotatedRect rrt = minAreaRect(contours[i]);
+		//rectangle(src_eqa, rrt.boundingRect(), 255);
+		Point2f pts[4];
+		rrt.points(pts);
+		int rrt_x = pts[1].x, rrt_y = pts[1].y;
+
+		uchar* pData = src_eqa.data;
+
+		int flag = 0;
+		for (size_t w = rrt_x; w < rrt.size.width + rrt_x; w++)
+		{
+			for (size_t h = rrt_y; h < rrt.size.height + rrt_y; h++)
+			{
+				// Calculate index of OpenCV Mat via: (row + x) * cols + (col + y)
+				int index = h * rrt.size.width + w;
+				if (pData[index] > 140)
+				{
+					flag = 1;
+					line(src_gray, Point(rrt_x, h), Point(rrt_x + rrt.size.width, h), 255);
+					int a = 0;
+					break;
+				}
+			}
+			if (flag == 1) { break; }
+		}
 	}
 
 	return 1;
