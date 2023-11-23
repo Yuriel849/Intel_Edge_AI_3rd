9008430de1d33ffc5c584912e707e717c5b177b0
DESKTOP-FA1PHE5\USER <yuriel849sp@gmail.com>
2023-11-23 15:16:08 +0900

    File renamed

--- ImageProcessing/VisionApp/ImageProcs/Original_Wafer_Detection.cpp
+++ ImageProcessing/VisionApp/ImageProcs/Original_Wafer_Detection.cpp
@@ -5,7 +5,6 @@ int main()
 	// File path to original wafer image
 	std::string filePath = "./res/img/wafer_original.png";
 
-	// Original image is in grayscale, so use as src
 	cv::Mat src = cv::imread(filePath, cv::ImreadModes::IMREAD_GRAYSCALE); // Grayscale image
 
 	// Check image is properly loaded
@@ -15,11 +14,7 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	cv::Mat src_bin = cv::Mat::zeros(src.size(), CV_8UC1);			   // Empty matrix for binarization
-	cv::Mat src_dx = cv::Mat::zeros(src.size(), CV_8UC1);
-	cv::Mat src_dy = cv::Mat::zeros(src.size(), CV_8UC1);
-	cv::Mat src_sobel = cv::Mat::zeros(src.size(), CV_8UC1);
-	cv::Mat src_sobel_f = cv::Mat::zeros(src.size(), CV_8UC1);
+	cv::Mat src_bin = cv::Mat::zeros(src.size(), CV_8UC1);	// Empty matrix for binarization
 
 	// Mask y-axis 157 - 719
 	cv::Mat mask = cv::Mat::zeros(src.size(), CV_8UC1);
@@ -30,14 +25,8 @@ int main()
 		for (size_t col = 0; col < src.cols; col++)
 		{
 			int index = row * src.cols + col;
-			if (157 < row && row < 719)
-			{
-				maskData[index] = 0;
-			}
-			else
-			{
-				maskData[index] = srcData[index];
-			}
+			if (157 < row && row < 719) { maskData[index] = 0; }
+			else { maskData[index] = srcData[index]; }
 		}
 	}
 
@@ -45,17 +34,33 @@ int main()
 	int thres = 125;
 	cv::threshold(mask, src_bin, thres, 255, ThresholdTypes::THRESH_BINARY);
 
+	// Morphology opening
 	int kernelSz = 10;
 	int shape = MorphShapes::MORPH_ELLIPSE;
 	cv::Size sz = Size(2 * kernelSz + 1, 2 * kernelSz + 1);
 	Mat SE = cv::getStructuringElement(shape, sz);
 
-	Mat src_closing;
+	Mat src_opening;
 	int type = MorphTypes::MORPH_OPEN;
-	cv::morphologyEx(src_bin, src_closing, type, SE);
-	Mat diff_closing = src_bin - src_closing;
+	cv::morphologyEx(src_bin, src_opening, type, SE);
+	Mat diff_closing = src_bin - src_opening;
 
-	int a = 0;
+	int a = 0; // Anchor for breakpoint
+
+	// Find contours
+	RNG rng(12345);
+	vector<vector<Point>> contours; //각 꼭지점 갯수 벡터배열에 저장
+	vector<Vec4i> hierarchy;
+	findContours(src_bin, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	Mat drawing = src.clone(); // Mat::zeros(src_color.size(), CV_3.8UC3);
+	cvtColor(drawing, drawing, COLOR_GRAY2RGB);
+	for (size_t i = 0; i < contours.size(); i++) //꼭지점 사이즈만큼 i 반복
+	{
+		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+		drawContours(drawing, contours, (int)i, color, 1, LINE_8, hierarchy, 0);
+	}
+
+	// Identify squares with bounding rectangle
 
 	int b = 0; // Anchor for breakpoint
 }
