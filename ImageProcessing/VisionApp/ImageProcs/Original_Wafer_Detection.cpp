842c5b31cd08647888ee80850ec23533f163fb64
DESKTOP-FA1PHE5\USER <yuriel849sp@gmail.com>
2023-11-23 16:49:58 +0900

    Add next steps as comments

--- ImageProcessing/VisionApp/ImageProcs/Original_Wafer_Detection.cpp
+++ ImageProcessing/VisionApp/ImageProcs/Original_Wafer_Detection.cpp
@@ -34,37 +34,36 @@ int main()
 	int thres = 125;
 	cv::threshold(mask, src_bin, thres, 255, ThresholdTypes::THRESH_BINARY);
 
-	// Morphology opening
+	// Morphology, opening
 	int kernelSz = 10;
 	int shape = MorphShapes::MORPH_ELLIPSE;
 	cv::Size sz = Size(2 * kernelSz + 1, 2 * kernelSz + 1);
 	Mat SE = cv::getStructuringElement(shape, sz);
-
 	Mat src_opening;
 	int type = MorphTypes::MORPH_OPEN;
 	cv::morphologyEx(src_bin, src_opening, type, SE);
 	Mat diff_closing = src_bin - src_opening;
 
-	int a = 0; // Anchor for breakpoint
-
 	// Find contours
 	RNG rng(12345);
-	vector<vector<Point>> contours; //각 꼭지점 갯수 벡터배열에 저장
+	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	findContours(src_opening, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	Mat drawing = src.clone(); // Mat::zeros(src_color.size(), CV_3.8UC3);
+	Mat drawing = src.clone();
 	cvtColor(drawing, drawing, COLOR_GRAY2RGB);
-	for (size_t i = 0; i < contours.size(); i++) //꼭지점 사이즈만큼 i 반복
+	for (size_t i = 0; i < contours.size(); i++)
 	{
 		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 		drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
 
+		// Identify squares with minimum area bounding rectangle
 		RotatedRect rrt = minAreaRect(contours[i]);
 		cv::rectangle(drawing, rrt.boundingRect2f().tl(), rrt.boundingRect2f().br(), CV_RGB(0, 255, 255), 4, LINE_8);
-
 	}
 
-	// Identify squares with bounding rectangle
+	// Differentiate wafer with defect squares from defectless squares
+	// Compare minimum area bounding rectangles and calculate the squared difference
+
 
 	int b = 0; // Anchor for breakpoint
 }
