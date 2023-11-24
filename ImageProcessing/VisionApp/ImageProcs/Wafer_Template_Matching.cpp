a5a7ffd2b1a7ec0333167c27afde4cf1f0791fff
DESKTOP-FA1PHE5\USER <yuriel849sp@gmail.com>
2023-11-24 16:02:22 +0900

    Detection of chip defect 1: overflowing squares

--- ImageProcessing/VisionApp/ImageProcs/Wafer_Template_Matching.cpp
+++ ImageProcessing/VisionApp/ImageProcs/Wafer_Template_Matching.cpp
@@ -3,62 +3,6 @@
 // Identical to function getROIs(const Mat& search_img, const Mat& ptrn_img, vector<Rect>& rois) in "23.ISP_Template_Matching_Img.cpp"
 void MatchingMethod(const Mat& serch_img, const Mat& ptrn_img, const double& thres, vector<Rect>& rois)
 {
-	/*
-	uchar* pSearch = serch_img.data;
-	int search_w = serch_img.cols;
-	int search_h = serch_img.rows;
-	uchar* pPtrn = ptrn_img.data;
-	int ptrn_w = ptrn_img.cols;
-	int ptrn_h = ptrn_img.rows;
-
-	Mat result = Mat::zeros(Size(search_w - ptrn_w + 1, search_h - ptrn_h + 1), CV_32FC1);
-	for (size_t row = 0; row < search_h - ptrn_h + 1; row++)
-	{
-		for (size_t col = 0; col < search_w - ptrn_w + 1; col++)
-		{
-			// Below code is executed at every pixel in "serch_img"
-			double TM_SQDIFF = 0.0;
-			double TM_SQDIFF_NORMED = 0.0;
-			for (size_t prow = 0; prow < ptrn_h; prow++)
-			{
-				for (size_t pcol = 0; pcol < ptrn_w; pcol++)
-				{
-					int search_index = (row + prow) * search_w + (col + pcol);
-					int ptrn_index = prow * ptrn_w + pcol;
-
-					// Calculate square difference
-					double diff = pSearch[search_index] - pPtrn[ptrn_index];
-					TM_SQDIFF += (diff * diff);
-				}
-			}
-			double ptrnSQ = 0., searchSQ = 0.;
-			for (size_t prow = 0; prow < ptrn_h; prow++)
-			{
-				for (size_t pcol = 0; pcol < ptrn_w; pcol++)
-				{
-					// Calculate sum of square of "serch_img" and "ptrn_img" for use when normalizing
-					int search_index = (row + prow) * search_w + (col + pcol);
-					int ptrn_index = prow * ptrn_w + pcol;
-					searchSQ += pSearch[search_index] * pSearch[search_index];
-					ptrnSQ += pPtrn[ptrn_index] * pPtrn[ptrn_index];
-				}
-			}
-
-			// If square difference is 0, means the image here is a perfect match for "ptrn_img"
-			//result.at<double>(row, col) = TM_SQDIFF;
-			//if (TM_SQDIFF == 0)
-			//	ptFind.push_back(Point(col, row));
-
-			// Alternative to above: Use TM_SQDIFF_NORMED instead of TM_SQDIFF
-			if (ptrnSQ == 0) ptrnSQ = 1;
-			TM_SQDIFF_NORMED = TM_SQDIFF / sqrt(ptrnSQ * searchSQ);
-			result.at<float>(row, col) = TM_SQDIFF_NORMED;
-			//if (TM_SQDIFF_NORMED <= 0.009)
-			//	rois.push_back(Rect(col, row, ptrn_w, ptrn_h));
-		}
-	}
-	*/
-
 	Mat result = Mat::zeros(Size(serch_img.cols - ptrn_img.cols + 1, serch_img.rows - ptrn_img.rows + 1), CV_32FC1);
 	int match_method = TM_CCORR_NORMED;
 	matchTemplate(serch_img, ptrn_img, result, match_method);
@@ -107,7 +51,9 @@ void MatchingMethod(const Mat& serch_img, const Mat& ptrn_img, const double& thr
 
 void main()
 {
-	std::string filePath_Search = "./res/img/wafer_full.png";
+	bool defectFlag = false;
+
+	std::string filePath_Search = "./res/img/location_black.png";
 	std::string filePath_Templt = "./res/img/wafer_template.png";
 
 	cv::Mat src_draw = cv::imread(filePath_Search, cv::ImreadModes::IMREAD_ANYCOLOR);
@@ -129,6 +75,96 @@ void main()
 		putText(src_draw, msg, finds[k].tl(), FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0, 255, 255), 1, 8);
 
 		cv::rectangle(src_filled, finds[k], CV_RGB(0, 205, 255), CV_FILLED);
+
+
+
+		cv::Mat src_templt = src_gray_templt.clone();
+		cv::Mat src_wafer = src_gray_search(finds[k]).clone();
+		cv::Mat src_tBin = cv::Mat::zeros(src_templt.size(), CV_8UC1);	// Empty matrix for binarization
+		cv::Mat src_wBin = cv::Mat::zeros(src_wafer.size(), CV_8UC1);	// Empty matrix for binarization
+
+		// For wafer : Mask y-axis 28 - 112
+		cv::Mat mask = cv::Mat::zeros(src_wafer.size(), CV_8UC1);
+		uchar* mData = mask.data;
+		for (size_t row = 0; row < src_wafer.rows; row++)
+		{
+			for (size_t col = 0; col < src_wafer.cols; col++)
+			{
+				int index = row * src_wafer.cols + col;
+				if (!(28 < row && row < 112)) { mData[index] = 255; }
+			}
+		}
+		cv::Mat src_wMasked = src_wafer & mask;
+
+		// For template : Mask y-axis 24 - 106
+		mask = cv::Mat::zeros(src_templt.size(), CV_8UC1);
+		mData = mask.data;
+		for (size_t row = 0; row < src_templt.rows; row++)
+		{
+			for (size_t col = 0; col < src_templt.cols; col++)
+			{
+				int index = row * src_templt.cols + col;
+				if (!(24 < row && row < 106)) { mData[index] = 255; }
+			}
+		}
+		cv::Mat src_tMasked = src_templt & mask;
+
+		// Binarization
+		thres = 125;
+		cv::threshold(src_wMasked, src_wBin, thres, 255, ThresholdTypes::THRESH_BINARY);
+		cv::threshold(src_tMasked, src_tBin, thres, 255, ThresholdTypes::THRESH_BINARY);
+
+		int kernelSz = 1;
+		int shape = MorphShapes::MORPH_ELLIPSE;
+		cv::Size sz = Size(2 * kernelSz + 1, 2 * kernelSz + 1);
+		Mat SE = cv::getStructuringElement(shape, sz);
+
+		//Ä§½Ä erode
+		cv::Mat src_wErode;
+		cv::Mat src_tErode;
+		cv::erode(src_wBin, src_wErode, SE);
+		cv::erode(src_tBin, src_tErode, SE);
+		Mat diff_wErode = src_wBin - src_wErode;
+		Mat diff_tErode = src_tBin - src_tErode;
+
+		// Find contours
+		vector<vector<Point>> wContours;
+		vector<vector<Point>> tContours;
+		vector<Vec4i> wHierarchy;
+		vector<Vec4i> tHierarchy;
+		findContours(src_wErode, wContours, wHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+		findContours(src_tErode, tContours, tHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+		Mat drawing_wafer = src_wafer.clone();
+		Mat drawing_templt = src_templt.clone();
+
+		if (wContours.size() == tContours.size())
+		{
+			for (size_t i = 0; i < wContours.size(); i++)
+			{
+				//drawContours(drawing, contours, (int)i, 50, 1, LINE_8, hierarchy, 0);
+				//drawContours(drawing, contours, (int)i, 50, 1, LINE_8, hierarchy, 0);
+				RotatedRect wRrt = minAreaRect(wContours[i]);
+				Rect2f wR2f = wRrt.boundingRect2f();
+				RotatedRect tRrt = minAreaRect(tContours[i]);
+				Rect2f tR2f = tRrt.boundingRect2f();
+				// Identify squares with minimum area bounding rectangle
+				cv::rectangle(drawing_wafer, wR2f.tl(), wR2f.br(), 0, 1, LINE_8);
+				cv::rectangle(drawing_templt, tR2f.tl(), tR2f.br(), 0, 1, LINE_8);
+
+				if (abs(tR2f.area() - wR2f.area()) > 100)
+				{
+					defectFlag = true;
+					break;
+				}
+			}
+		}
+
+		if (defectFlag)
+		{
+			cv::rectangle(src_filled, finds[k], CV_RGB(255, 0, 100), CV_FILLED);
+			defectFlag = false;
+		}
+
 	}
 	int a = 0;
 }
